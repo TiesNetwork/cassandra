2f7df025885f8912b90fc3f893d5ecfc606995cf
Vincent Royer <vroyer@strapdata.com>
2017-04-13 15:49:02 +0200

    CASSANDRA-12837 Add multi-threaded support to nodetool rebuild_index
     Add multi-threaded support to nodetool rebuild_index.

--- src/java/org/apache/cassandra/index/SecondaryIndexManager.java
+++ src/java/org/apache/cassandra/index/SecondaryIndexManager.java
@@ -245,7 +245,11 @@ public class SecondaryIndexManager implements IndexRegistry
     * @param sstables the data to build from
     * @param indexNames the list of indexes to be rebuilt
     */
-    public void rebuildIndexesBlocking(Collection<SSTableReader> sstables, Set<String> indexNames)
+    public void rebuildIndexesBlocking(Collection<SSTableReader> sstables, Set<String> indexNames) {
+        rebuildIndexesBlocking(1, sstables, indexNames);
+    }
+    
+    public void rebuildIndexesBlocking(int indexThreads, Collection<SSTableReader> sstables, Set<String> indexNames)
     {
         Set<Index> toRebuild = indexes.values().stream()
                                                .filter(index -> indexNames.contains(index.getIndexMetadata().name))
@@ -259,7 +263,7 @@ public class SecondaryIndexManager implements IndexRegistry
 
         toRebuild.forEach(indexer -> markIndexRemoved(indexer.getIndexMetadata().name));
 
-        buildIndexesBlocking(sstables, toRebuild);
+        buildIndexesBlocking(indexThreads, sstables, toRebuild);
 
         toRebuild.forEach(indexer -> markIndexBuilt(indexer.getIndexMetadata().name));
     }
@@ -358,6 +362,11 @@ public class SecondaryIndexManager implements IndexRegistry
     }
 
     private void buildIndexesBlocking(Collection<SSTableReader> sstables, Set<Index> indexes)
+    {
+        buildIndexesBlocking(1, sstables, indexes);
+    }
+    
+    private void buildIndexesBlocking(int indexThreads, Collection<SSTableReader> sstables, Set<Index> indexes)
     {
         if (indexes.isEmpty())
             return;
